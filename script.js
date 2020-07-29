8be025c1abd9dd34355c9a116069d6064073c3fc
Chester <chesterdeveloper2000@gmail.com>
2020-07-29 10:10:21 +0800

    clicking on the video itself plays and pauses the video.. also when clicking on the play btn.. however, play btn doesn't turns into a pause btn yet when paused

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-
+const video = document.querySelector("video")
+const progressRange = document.querySelector(".progress-range")
+const progressBar = document.querySelector(".progress-bar")
+const playBtn = document.getElementById("play-btn")
+const volumeIcon = document.getElementById("volume-icon")
+const volumeRange = document.querySelector(".volume-range")
+const volumeBar = document.querySelector(".volume-bar")
+const currentTime = document.querySelector(".time-elapsed")
+const duration = document.querySelector(".time-duration")
+const fullscreenBtn = document.querySelector(".fullscreen")
 
 // Play & Pause ----------------------------------- //
-
+togglePlay = () => {
+    if(video.paused) {
+        video.play()
+    } else {
+        video.pause()
+    }
+}
 
 
 // Progress Bar ---------------------------------- //
@@ -19,3 +34,8 @@
 // Fullscreen ------------------------------- //
 
 
+
+
+// event listeners
+playBtn.addEventListener("click", togglePlay)
+video.addEventListener("click", togglePlay)
